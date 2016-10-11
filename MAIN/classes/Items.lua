40ef399d077d5d99304afa2e9827c1ae485e6489
AnthonyAllan <aaustin1@ycp.edu>
2016-10-11 17:59:29 -0400

    Forgot to commit items

---
+++ MAIN/classes/Items.lua
@@ -0,0 +1,66 @@
+module (..., package.seeall)
+-- Image Variables for Items
+local healthImage = "Health.png"
+local manaImage = "Mana.png"
+local keyImage = "Key.png"
+local doorImage = "Door_.png"
+local fdoorImage = "fdoor.png"
+
+-- Variable to store Items
+local items = display.newGroup()
+
+function newItems ( arg, x, y )
+  -- Health
+  -- Restores Health
+  if(arg == "hp") then
+    items.hp = display.newImage( healthImage, x, y )
+    items:insert(items.hp)
+    physics.addBody( items.hp, "static" )
+    items.hp.myName = "hp"
+  end
+  -- Mana
+  -- Replenishes Mana
+  if(arg == "mana") then
+    items.mana = display.newImage( manaImage, x, y )
+    items:insert(items.mana)
+    physics.addBody( items.mana, "static" )
+    items.mana.myName = "mana"
+  end
+
+  -- Key
+  -- Unlocks Doors
+  if (arg == "key") then
+    items.key = display.newImage( keyImage, x, y )
+    items:insert(items.key)
+    physics.addBody( items.key, "static")
+    items.key.myName = "key"
+  end
+
+  -- Final Door
+  -- Ends Level
+  if (arg == "fdoor") then
+    items.fdoor = display.newImage( fdoorImage, x, y )
+    items:insert(items.fdoor)
+    physics.addBody( items.fdoor, "static")
+    items.fdoor.myName = "fdoor"
+  end
+
+  -- Door
+  -- Locked, unlocked by using a key
+  if (arg == "door") then
+    items.door = display.newImage( doorImage, x, y )
+    items:insert(items.door)
+    items.door.circle = display.newCircle( x, y - 20, 8)
+    items.door.circle:setFillColor(1,0,0)
+    items:insert(items.door.circle)
+    physics.addBody( items.door, "static")
+    items.door.myName = "door"
+  end
+
+  function items:destroy()
+    display.remove(self)
+    self:removeSelf()
+  end
+
+  return items
+end
