a7d20fb323655fc2a36e7e3a4a3c135dc517ebb4
Michael Trupia <mtrupia@ycp.edu>
2016-11-28 12:00:07 -0500

    fixed score stuff

--- MAIN/levels/player.lua
+++ MAIN/levels/player.lua
@@ -1,11 +1,10 @@
 return {
-<<<<<<< HEAD
-	levels = 1,
-	health = 100,
+	levels = 5,
+	health = 150,
 	mana = 100,
-	level1 = {score = 0, items = 0},
-	level2 = {score = 0, items = 0},
-	level3 = {score = 0, items = 0},
-	level4 = {score = 0, items = 0},
-	level5 = {score = 0, items = 0}
+	level1 = {score = 1700, items = 0},
+	level2 = {score = 1000, items = 0},
+	level3 = {score = 1200, items = 0},
+	level4 = {score = 1100, items = 0},
+	level5 = {score = 1600, items = 0}
 }
