cead14ab15f6a33ee7c967dd4039d1418042052d
Michael Trupia <mtrupia@ycp.edu>
2016-12-05 12:37:18 -0500

    nothing

--- MAIN/main.lua
+++ MAIN/main.lua
@@ -24,8 +24,8 @@ _G.borders = 140
 -- Physics and filters
 _G.physics = require("physics")
 _G.worldCollisionFilter 		 = {	categoryBits = 1, maskBits = 30 }
---_G.playerCollisionFilter 		 = { categoryBits = 2, maskBits = 57 }
-_G.playerCollisionFilter 		 = { categoryBits = 2, maskBits = 56 }
+_G.playerCollisionFilter 		 = { categoryBits = 2, maskBits = 57 }
+--_G.playerCollisionFilter 		 = { categoryBits = 2, maskBits = 56 }
 _G.powerCollisionFilter 		 = { categoryBits = 4, maskBits = 9 }
 _G.enemyCollisionFilter 		 = { categoryBits = 8, maskBits = 47 }
 _G.enemyPowerCollisionFilter = { categoryBits = 16, maskBits = 35 }
