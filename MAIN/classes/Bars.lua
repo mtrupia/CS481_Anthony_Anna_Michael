7bf93e5e0a26596f103dad488208f15d2ff60b4c
Michael Trupia <mtrupia@ycp.edu>
2016-12-15 19:19:13 -0500

    Fixing bars

--- MAIN/classes/Bars.lua
+++ MAIN/classes/Bars.lua
@@ -7,16 +7,16 @@ local healthSpriteOptions = {
 local healthSpriteSheet = graphics.newImageSheet( "images/healthBar.png", healthSpriteOptions )
 local healthSpriteData = {
   { name = "0", start=1, count=1, time=0,   loopCount=1 },
-  { name = "10", start=2, count=1, time=0, loopCount=1 },
-  { name = "20", start=3, count=1, time=0, loopCount=1 },
-  { name = "30", start=4, count=1, time=0, loopCount=1 },
-  { name = "40", start=5, count=1, time=0, loopCount=1 },
-  { name = "50", start=6, count=1, time=0,   loopCount=1 },
-  { name = "60", start=7, count=1, time=0, loopCount=1 },
-  { name = "70", start=8, count=1, time=0, loopCount=1 },
-  { name = "80", start=9, count=1, time=0, loopCount=1 },
-  { name = "90", start=10, count=1, time=0, loopCount=1 },
-  { name = "100", start=11, count=1, time=0, loopCount=1 }
+  { name = "1", start=2, count=1, time=0, loopCount=1 },
+  { name = "2", start=3, count=1, time=0, loopCount=1 },
+  { name = "3", start=4, count=1, time=0, loopCount=1 },
+  { name = "4", start=5, count=1, time=0, loopCount=1 },
+  { name = "5", start=6, count=1, time=0,   loopCount=1 },
+  { name = "6", start=7, count=1, time=0, loopCount=1 },
+  { name = "7", start=8, count=1, time=0, loopCount=1 },
+  { name = "8", start=9, count=1, time=0, loopCount=1 },
+  { name = "9", start=10, count=1, time=0, loopCount=1 },
+  { name = "10", start=11, count=1, time=0, loopCount=1 }
 }
 local manaSpriteOptions = {
   width = 60,
@@ -26,57 +26,42 @@ local manaSpriteOptions = {
 local manaSpriteSheet = graphics.newImageSheet( "images/manaBar.png", manaSpriteOptions)
 local manaSpriteData = {
   { name = "0",  start=1, count=1,  time=0,   loopCount=1 },
-  { name = "10", start=2, count=1, time=0,   loopCount=1 },
-  { name = "20", start=3, count=1, time=0,   loopCount=1 },
-  { name = "30", start=4, count=1, time=0,   loopCount=1 },
-  { name = "40", start=5, count=1, time=0,   loopCount=1 },
-  { name = "50", start=6, count=1, time=0,   loopCount=1 },
-  { name = "60", start=7, count=1, time=0,   loopCount=1 },
-  { name = "70", start=8, count=1, time=0,   loopCount=1 },
-  { name = "80", start=9, count=1, time=0,   loopCount=1 },
-  { name = "90", start=10, count=1, time=0,  loopCount=1 },
-  { name = "100", start=11, count=1, time=0, loopCount=1 }
+  { name = "1", start=2, count=1, time=0,   loopCount=1 },
+  { name = "2", start=3, count=1, time=0,   loopCount=1 },
+  { name = "3", start=4, count=1, time=0,   loopCount=1 },
+  { name = "4", start=5, count=1, time=0,   loopCount=1 },
+  { name = "5", start=6, count=1, time=0,   loopCount=1 },
+  { name = "6", start=7, count=1, time=0,   loopCount=1 },
+  { name = "7", start=8, count=1, time=0,   loopCount=1 },
+  { name = "8", start=9, count=1, time=0,   loopCount=1 },
+  { name = "9", start=10, count=1, time=0,  loopCount=1 },
+  { name = "10", start=11, count=1, time=0, loopCount=1 }
 }
 Bar = class ('Bar')
 function Bar:initialize(props)
-  self.healthPos = {}
-  self.manaPos = {}
-  local healthPos = self.healthPos
-  local manaPos = self.manaPos
-
-
-  healthPos.x       = props.healthX or screenW - 460
-  healthPos.y       = props.healthY or screenH - 300
-  healthPos.scaleX  = props.healthScaleX or 2
-  healthPos.scaleY  = props.healthScaleY or 1
-
-  manaPos.x = props.manaX or screenW - 335
-  manaPos.y = props.manaY or screenH - 300
-  manaPos.scaleX = props.manaScaleX or 2
-  manaPos.scaleY = props.manaScaleY or 1
-
   self.target = props.target
-
-
   self.sprite = props.sprite
 end
 
 function Bar:show()
   self.sprite = display.newGroup()
   local sprite = self.sprite
-  local healthPos = self.healthPos
-  local manaPos = self.manaPos
+  
   -- Health Bar
-  sprite.healthBar = display.newText(sprite,self.target.health .. " / " .. self.target.maxHealth, display.contentWidth/2, display.contentHeight/2+110, native.systemFont, 24)
-  sprite.healthBar.x = healthPos.x
-  sprite.healthBar.y = healthPos.y
-  sprite.healthBar:setFillColor(1,0,0)
+  sprite.healthBar = display.newSprite(sprite, healthSpriteSheet, healthSpriteData)
+  sprite.healthBar.x = screenW - 460
+  sprite.healthBar.y = screenH - 300
+  sprite.healthBar:scale(2,1)
+  sprite.healthBar:setSequence("10")
+  sprite.healthBar:play()
   -- Mana Bar
-  sprite.manaBar = display.newText(sprite,self.target.mana .. " / " .. self.target.maxMana, display.contentWidth/2, display.contentHeight/2+110, native.systemFont, 24)
-  sprite.manaBar.x = manaPos.x
-  sprite.manaBar.y = manaPos.y
-  sprite.manaBar:setFillColor(0,0,1)
-
+  sprite.manaBar = display.newSprite(sprite, manaSpriteSheet, manaSpriteData)
+  sprite.manaBar.x = screenW - 335
+  sprite.manaBar.y = screenH - 300
+  sprite.manaBar:scale(2,1)
+  sprite.manaBar:setSequence("10")
+  sprite.manaBar:play()
+  
   -- Score
   sprite.score = display.newText(self.target.score, screenW - 100, screenH - 305)
   sprite.score:setFillColor( 1, 0, 0.5 )
@@ -92,24 +77,18 @@ function Bar:show()
 end
 
 function Bar:setHealth(amt)
-  local player = self.target
-  local sprite = self.sprite
-  -- Update Health
-  player.health = player.health + amt
-  -- Upper and Lower Bounds for Player Health
-  if player.health < 0 then player.health = 0
-  elseif player.health >= player.maxHealth then player.health = player.maxHealth end
-
-  if sprite then
-    sprite.healthBar.text = self.target.health .. " / " .. self.target.maxHealth
-  end
-  
-  if player.name == 'Enemy' then
+	local player = self.target
+	local sprite = self.sprite
+	-- Update Health
+	player.health = player.health + amt
+	-- Upper and Lower Bounds for Player Health
+	if player.health < 0 then player.health = 0
+	elseif player.health >= player.maxHealth then player.health = player.maxHealth end
+	
 	if sprite then
-		sprite.healthBar:setSequence(player.health)
+		sprite.healthBar:setSequence(player.health / (player.maxHealth/10))
 		sprite.healthBar:play()
 	end
-  end
 end
 
 function Bar:setMana(amt)
@@ -122,7 +101,8 @@ function Bar:setMana(amt)
   elseif player.mana >= player.maxMana then player.mana = player.maxMana end
 
   if sprite then
-    sprite.manaBar.text = self.target.mana .. " / " .. self.target.maxMana
+    sprite.manaBar:setSequence(player.mana / (player.maxMana/10))
+	sprite.manaBar:play()
   end
 end
 
@@ -149,7 +129,7 @@ function eBar:show()
   sprite.healthBar.x = target.x
   sprite.healthBar.y = target.y - 20
   sprite.healthBar:scale(self.scaleX, self.scaleY)
-  sprite.healthBar:setSequence("100")
+  sprite.healthBar:setSequence("10")
   sprite.healthBar:play()
 end
 
